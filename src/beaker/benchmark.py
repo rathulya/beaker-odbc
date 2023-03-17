d7deb4212d8c5e52b85bb62b030247089533903e
Michael O'Kane <michael.okane@databricks.com>
2023-03-17 15:10:40 +0000

    Revert "Revert "Add setName() function and temp view creation.""
    This reverts commit 534cca3886e23aa02fc4a9a73c76fb962d239f84.

--- src/beaker/benchmark.py
+++ src/beaker/benchmark.py
@@ -14,9 +14,10 @@ from beaker.spark_fixture import get_spark_session
 class Benchmark:
     """Encapsulates a query benchmark test."""
 
-    def __init__(self, query=None, query_file=None, query_file_dir=None, concurrency=1, db_hostname=None,
+    def __init__(self, name="Beaker Benchmark Test", query=None, query_file=None, query_file_dir=None, concurrency=1, db_hostname=None,
                  warehouse_http_path=None, token=None, catalog='hive_metastore', new_warehouse_config=None,
                  results_cache_enabled=False):
+        self.name = self._clean_name(name)
         self.query = query
         self.query_file = query_file
         self.query_file_dir = query_file_dir
@@ -52,6 +53,15 @@ class Benchmark:
         warehouse_utils.setToken(token=self.token)
         warehouse_utils.setHostname(hostname=self.hostname)
         return warehouse_utils.launch_warehouse(self.new_warehouse_config)
+
+    def _clean_name(self, name):
+        """Replaces spaces with underscores"""
+        name_lowered_stripped = name.lower().strip()
+        return re.sub(r"\s+", "_", name_lowered_stripped)
+
+    def setName(self, name):
+        """Sets the name of the Benchmark Test."""
+        self.name=self._clean_name(name)
 
     def setWarehouseConfig(self, config):
         """Launches a new cluster/warehouse from a JSON config."""
@@ -215,6 +225,7 @@ class Benchmark:
         object_str = f"""
     Benchmark Test:
     ------------------------
+    name={self.name}
     catalog={self.catalog}
     query="{self.query}"
     query_file={self.query_file}
